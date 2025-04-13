9adc899dc3618418c8ecc987cf6c11132b087c61
pedrovyg <pedrovygotsky010@gmail.com>
2025-04-13 15:12:06 -0300

    Primeiro commit do projeto Decola Tech Avanade 2025

---
+++ src/main/java/com/pedro/decola_tech_avanade_2025/Application.java
@@ -0,0 +1,13 @@
+package com.pedro.decola_tech_avanade_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedro/decola_tech_avanade_2025/DecolaTechAvanade2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.decola_tech_avanade_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechAvanade2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
